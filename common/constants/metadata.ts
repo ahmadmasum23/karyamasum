7007ef2799d61e064c254136fd3f8fb0bdc31d8a
Ahmad Masum <ahmadmasum0723@gmail.com>
2026-02-28 05:09:09 +0700

    add CEO

--- common/constants/metadata.ts
+++ common/constants/metadata.ts
@@ -1,7 +1,18 @@
 export const METADATA = {
   creator: "Ahmad Ma'sum",
-  description: "Personal website, portfolio, blog",
-  keyword: "masum, Ahmad Ma'sum",
+  title: "Ahmad Ma'sum | Frontend Developer Indonesia | SMK Brantas Karangkates",
+  description:
+    "Ahmad Ma'sum is a Frontend & Mobile Developer from Indonesia and SMK Brantas graduate. Specializing in Next.js, React, and Flutter development.",
+  keywords: [
+    "Ahmad Ma'sum",
+    "Ahmad Masum",
+    "Ahmad Masum SMK Brantas",
+    "Ahmad Ma'sum SMK Brantas",
+    "Frontend Developer Indonesia",
+    "Mobile Developer Indonesia",
+    "SMK Brantas",
+    "Masum Programmer",
+  ],
   authors: {
     name: "Ahmad Ma'sum",
     url: process.env.DOMAIN || process.env.NEXTAUTH_URL || "http://localhost:3000",
